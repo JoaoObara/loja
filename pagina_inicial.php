48dc98dc667761e050bdbd2bdb7785d8a7fb3ef7
joao <joao.ricardo.obara@gmail.com>
2022-05-25 22:13:23 -0400

    adicionando imagem

--- pagina_inicial.php
+++ pagina_inicial.php
@@ -121,7 +121,7 @@ include INC_DIR . 'header.inc';
                         <p class="lead">Alguns ótimos conteúdos de espaço reservado para o primeiro featurette aqui. Imagine uma prosa excitante aqui.</p>
                     </div>
                     <div class="col-md-5">
-                        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false" >
                             <title>Placeholder</title>
                             <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
                         </svg>
