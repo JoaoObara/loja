3a53a1f596934c516b721e6f8ae6efb868bd739c
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-06-01 22:16:19 -0400

    imagem svg

--- estoque.php
+++ estoque.php
@@ -1,101 +1,69 @@
 <?php
 // include './includes/head.inc'; 
-session_start();
 include 'UI_include.php';
+include INC_DIR . "valida_sessao.inc";
 include INC_DIR . 'header.inc';
 ?>
 
 <!-- PARTE 1 -->
+
 <body>
-  <div class="container">
-    <?php include INC_DIR . 'menu.inc'; ?>
-  </div>
+    <div class="container">
+        <?php include INC_DIR . 'menu.inc'; ?>
+    </div>
 
-  <h3 style="text-align: center;">Estoque de carros</h3>
-<table class="table">
-<thead>
-<tr>
-<th>Marca</th>
-<th>Modelo</th>
-<th>Descrição</th>
-<th>Mod/Fab</th>
-<th>Cor</th>
-<th>Placa</th>
-<th>Preço</th>
-<th>Ações</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>Ford</td>
-<td>Ka</td>
-<td>1.0 MPI 8V Flex 2P MANUAL</td>
-<td>2019/2019</td>
-<td>branco</td>
-<td>GHF3453</td>
-<td>R$ 40.225,00</td>
-<td class="td-actions text-right">
-<button type="button" rel="tooltip" class="btn-sm btn-info">
-<i class="material-icons">person</i>
-</button>
+    <h3 style="text-align: center;">Estoque de carros</h3>
+    <table class="container table">
+        <thead>
+            <tr>
+                <th>Marca</th>
+                <th>Modelo</th>
+                <th>Descrição</th>
+                <th>Mod/Fab</th>
+                <th>Cor</th>
+                <th>Placa</th>
+                <th>Preço</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            include INC_DIR . "conecta_mysqli.inc";
+            $sql = "SELECT carros.id_carro, marcas.desc_marca, carros.modelo, carros.descricao,
+carros.mod_fab, carros.cor, carros.placa, carros.valor FROM marcas,carros WHERE carros.marca =
+marcas.id_marca ORDER BY carros.marca ASC, carros.modelo ASC,carros.mod_fab ASC;";
+            $resultado = mysqli_query($conexao, $sql);
+            while ($dados = mysqli_fetch_array($resultado)) :
+            ?>
+                <tr>
+                    <td><?php echo $dados['desc_marca']; ?></td>
+                    <td><?php echo $dados['modelo']; ?></td>
+                    <td><?php echo $dados['descricao']; ?></td>
+                    <td><?php echo $dados['mod_fab']; ?></td>
+                    <td><?php echo $dados['cor']; ?></td>
+                    <td><?php echo $dados['placa']; ?></td>
+                    <td><?php echo $dados['valor']; ?></td>
+                    <td class="td-actions text-right">
+                        <button type="button" rel="tooltip" class="btn-sm btn-secondary">
+                            <i class="material-icons">person</i>
+                        </button>
 
-<!-- PARTE 2 -->
-<button type="button" rel="tooltip" class="btn-sm btn-success">
-<i class="material-icons">edit</i>
-</button>
-<button type="button" rel="tooltip" class="btn-sm btn-danger">
-<i class="material-icons">close</i>
-</button>
-</td>
-</tr>
-<tr>
-<td>Toyota</td>
-<td>Corola</td>
-<td>1.8 SE-G 16V FLEX 4P AUTOMÁTICO</td>
-<td>2021/2022</td>
-<td>prata</td>
-<td>BHF3A53</td>
-<td>R$ 100.100,00</td>
-<td class="td-actions text-right">
-<button type="button" rel="tooltip" class="btn-sm btn-info btn-round">
-<i class="material-icons">person</i>
-</button>
-<button type="button" rel="tooltip" class="btn-sm btn-success btn-round">
-<i class="material-icons">edit</i>
-</button>
-<button type="button" rel="tooltip" class="btn-sm btn-danger btn-round">
-<i class="material-icons">close</i>
-</button>
-</td>
+                        <a href="editar.php?id=<?php echo $dados['id_carro']; ?>">
+                            <button type="button" rel="tooltip" class="btn-sm btn-success">
+                                <i class="material-icons">edit</i>
+                            </button></a><a href="deletar.php?id=<?php echo $dados['id_carro']; ?>">
+                            <button type="button" rel="tooltip" class="btn-sm btn-danger">
+                                <i class="material-icons">close</i>
+                            </button></a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 
-<!-- PARTE 3 -->
-</tr>
-<tr>
-<td>Chevrolet</td>
-<td>Onix</td>
-<td>1.0 FLEX 4P</td>
-<td>2022/2022</td>
-<td>Branco</td>
-<td>FRF7A52</td>
-<td>R$ 50.400,00</td>
-<td class="td-actions text-right">
-<button type="button" rel="tooltip" class="btn-sm btn-info">
-<i class="material-icons">person</i>
-</button>
-<button type="button" rel="tooltip" class="btn-sm btn-success">
-<i class="material-icons">edit</i>
-</button>
-<button type="button" rel="tooltip" class="btn-sm btn-danger">
-<i class="material-icons">close</i>
-</button>
-</td>
-</tr>
-</tbody>
-</table>
-
-  <?php
-  include INC_DIR . 'foot.inc';
-  ?>
+    <?php
+    include INC_DIR . 'foot.inc';
+    ?>
 </body>
 
 </html>
