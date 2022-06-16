586f92d3174eb421eaf47ff6a391def51681378e
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-06-15 22:22:36 -0400

    part 2

--- index.php
+++ index.php
@@ -10,10 +10,50 @@ include INC_DIR . 'header.inc';
     <?php include INC_DIR . 'menu.inc'; ?>
   </div>
 
-  <div class='container'>
-    <h2>Realize o login para acessar o sistema</h2>
-    <image src='assets/images/car.jpg'></image>
-  </div>
+  <main>
+    <div id="mycarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+        aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" 
+          aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+          <img src="./assets/images/slide1_azul.png">
+          <rect width="100%" height="100%" fill="#777" /> 
+          </svg>
+
+          <div class="container">
+            <div class="carousel-caption text-start">
+              <h1>Seu sonho. Aqui.</h1>
+              <p>
+                Seu carro dos sonhos é acessível.
+              </p>
+              <p>
+                <a class="btn btn-lg btn-primary" href="registre.php">Registre-se</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+          <img src="./assets/images/slide2_cinza.png">
+          <rect width="100%" height="100%" fill="#777" />
+          </svg>
+
+          <div class="container"> 
+            <div class="carousel-caption text-start">
+              <h1>Preços imbatíveis.</h1>
+              <p>
+                Aproveite as taxas de 0,4% ao mês
+              </p>
+  </main>
 
   <?php
   include INC_DIR . 'foot.inc';
