2b7eb8d6706851f79a258dc53f35b60b259ec9ab
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-06-18 10:58:36 -0400

    slide 15

--- loginp.php
+++ loginp.php
@@ -6,7 +6,7 @@ $senhaencriptada = md5($senha);
 // acesso ao banco de dados
 include "UI_include.php";
 include INC_DIR . 'conecta_mysqli.inc';
-$resultado = mysqli_query($conexao, "SELECT u_senha FROM usuarios where u_usuario='$usuario'");
+$resultado = mysqli_query($conexao, "SELECT u_senha, u_cat, u_avatar FROM usuarios where u_usuario='$usuario'");
 $linhas = mysqli_num_rows($resultado);
 if ($linhas == 0) // testa se a consulta retornou algum registro
 {
@@ -17,20 +17,26 @@ if ($linhas == 0) // testa se a consulta retornou algum registro
     echo "</body></html>";
 } else {
     $linha = mysqli_fetch_array($resultado);
+    $categoria = $linha['u_cat'];
+    $avatar = $linha['u_avatar'];
     if ($senhaencriptada !== $linha['u_senha']) // confere senha
     {
-        echo "<html><body>";
+/*         echo "<html><body>";
         echo "<p>A senha está incorreta!</p>";
         echo $senhaencriptada;
         echo "<br/>";
         echo $linha['u_senha'];
         echo "<p><a href=\"login.php\">Voltar</a></p>";
-        echo "</body></html>";
+        echo "</body></html>"; */
+        mysqli_close($conexao);
+        echo "<script> location.replace('login.php?erro');</script>";
     } else // usuário e senha corretos. Vamos criar as variáveis da sessão
     {
         session_start();
         $_SESSION['usuario'] = $usuario;
         $_SESSION['senha'] = $senhaencriptada;
+        $_SESSION['categoria'] = $categoria;
+        $_SESSION['avatar'] = $avatar;
         // direciona para a página inicial dos usuários cadastrados
         echo "<script> location.replace('pagina_inicial.php'); </script>";
         exit();
