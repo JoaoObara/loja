0522f348028402205675ab0081eca3c29f013242
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-06-20 21:12:01 -0400

    alterando notificação do estoque

--- estoque.php
+++ estoque.php
@@ -11,28 +11,56 @@ include INC_DIR . 'header.inc';
 
         <?php
         if (isset($_GET['incsucesso'])) :
-            echo "<div class='alert alert-success' role='alert'>";
-            echo "NOVO CARRO INCLUÍDO COM SUCESSO";
-            echo "</div>";
+            echo '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            </svg><div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              Novo carro incluído com sucesso
+            </div>
+          </div>';
         endif;
 
         if (isset($_GET['altsucesso'])) :
-            echo "<div class='alert alert-success' role='alert'>";
-            echo "CARRO ALTERADO COM SUCESSO";
-            echo "</div>";
+            echo '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            </svg><div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              Carro alterado com sucesso
+            </div>
+          </div>';
         endif;
 
         if (isset($_GET['delsucesso'])) :
-            echo "<div class='alert alert-success' role='alert'>";
-            echo "CARRO EXCLUÍDO COM SUCESSO";
-            echo "</div>";
+            echo '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            </svg><div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              Carro excluído com sucesso
+            </div>
+          </div>';
         endif;
 
         if (isset($_GET['erro'])) :
-            echo "<div class='alert alert-danger' role='alert'>";
-            echo "ERRO DETECTADO";
-            echo "</div>";
-        endif;
+            echo '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </symbol>
+            </svg><div class="alert alert-danger d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+            <div>
+              Erro detectado!!
+            </div>
+          </div>';
+        endif; 
         ?>
     </div>
 
@@ -54,8 +82,8 @@ include INC_DIR . 'header.inc';
             <?php
             include INC_DIR . "conecta_mysqli.inc";
             $sql = "SELECT carros.id_carro, marcas.desc_marca, carros.modelo, carros.descricao,
-carros.mod_fab, carros.cor, carros.placa, carros.valor FROM marcas,carros WHERE carros.marca =
-marcas.id_marca ORDER BY carros.marca ASC, carros.modelo ASC,carros.mod_fab ASC;";
+            carros.mod_fab, carros.cor, carros.placa, carros.valor FROM marcas,carros WHERE carros.marca =
+            marcas.id_marca ORDER BY carros.marca ASC, carros.modelo ASC,carros.mod_fab ASC;";
             $resultado = mysqli_query($conexao, $sql);
             while ($dados = mysqli_fetch_array($resultado)) :
             ?>
@@ -68,6 +96,7 @@ marcas.id_marca ORDER BY carros.marca ASC, carros.modelo ASC,carros.mod_fab ASC;
                     <td><?php echo $dados['placa']; ?></td>
                     <td><?php echo $dados['valor']; ?></td>
                     <td class="td-actions text-right">
+                        <a href="fotos.php?id=<?php echo $dados['id_carro']; ?>">
                         <button type="button" rel="tooltip" class="btn-sm btn-secondary">
                             <i class="material-icons">person</i>
                         </button>
